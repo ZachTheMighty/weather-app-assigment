be53f05434643cec655d91f7b0227be3567a83ed
ZachTheMighty <promoted66@gmail.com>
2026-06-25 14:14:18 +0000

    chore: removed code for debugging

--- src/modules/get_hour.js
+++ src/modules/get_hour.js
@@ -1,9 +1,10 @@
 import fetchData from "./fetch_data.js";
 
-export default async function fn(day, hour, location) {
+export default async function (day, hour, location) {
   const data = await fetchData(location);
   const hourInfo = data.days[day].hours[hour];
   const { datetime, temp, feelslike, humidity, conditions, icon } = hourInfo;
+
   return {
     datetime,
     temp,
@@ -13,5 +14,3 @@ export default async function fn(day, hour, location) {
     icon,
   };
 }
-
-fn(0, 16, "amman").then((data) => console.log(data));
