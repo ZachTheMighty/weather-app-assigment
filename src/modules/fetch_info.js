4ca0c1b2b8e192e2e6ab6de2bb4954c21416469e
ZachTheMighty <promoted66@gmail.com>
2026-06-25 12:57:41 +0000

    add: function for fetching weather info as json

---
+++ src/modules/fetch_info.js
@@ -0,0 +1,10 @@
+async function fetchData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=ZK4WAXABMLFLA7LWGZZBYK5F2`,
+    );
+    return response.json();
+  } catch (error) {
+    throw new Error("enter a valid location");
+  }
+}
