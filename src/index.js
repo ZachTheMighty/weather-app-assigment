ad42d0e364861cd37290008fae50c5be53b89d0c
ZachTheMighty <promoted66@gmail.com>
2026-06-25 15:46:02 +0000

    add: method for displaying weather for entered country in console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./index.css";
+import fetchData from "./modules/fetch_data.js";
+import getDay from "./modules/get_day.js";
+
+function displayWeather() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const fetchedData = fetchData(form.country.value);
+    getDay(0, fetchedData).then((data) => console.log(data));
+  });
+}
+
+displayWeather();
