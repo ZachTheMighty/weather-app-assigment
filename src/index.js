01dd49e1b7e43f913c17c1a153516b7f99fbba29
ZachTheMighty <promoted66@gmail.com>
2026-06-25 16:12:29 +0000

    feat: added error handling for entered location

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ function displayWeather() {
   const form = document.querySelector("form");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const fetchedData = fetchData(form.country.value);
-    getDay(0, fetchedData).then((data) => console.log(data));
+
+    const fetchedData = fetchData(form.location.value);
+
+    getDay(0, fetchedData)
+      .then((data) => console.log(data))
+      .catch(() => {
+        form.location.setCustomValidity("enter a valid location");
+        form.location.reportValidity();
+      });
+  });
+
+  form.location.addEventListener("input", () => {
+    form.location.setCustomValidity("");
   });
 }
 
--- src/modules/fetch_data.js
+++ src/modules/fetch_data.js
@@ -1,10 +1,6 @@
 export default async function (location) {
-  try {
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=ZK4WAXABMLFLA7LWGZZBYK5F2`,
-    );
-    return response.json();
-  } catch (error) {
-    throw new Error("enter a valid location");
-  }
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=ZK4WAXABMLFLA7LWGZZBYK5F2`,
+  );
+  return response.json();
 }
