088f9836331a05ef459858c814346b412be4ca00
ZachTheMighty <promoted66@gmail.com>
2026-06-26 16:30:58 +0000

    add: function to convert time from 24 system to AM PM

---
+++ src/modules/convert_to_am_pm.js
@@ -0,0 +1,16 @@
+export default function fn(time) {
+  const splitTime = time.split(":");
+  let [hours, minutes, seconds] = splitTime;
+  let symbol;
+
+  if (+hours > 0 && +hours < 12) symbol = "AM";
+  else if (+hours === 0) {
+    hours = "12";
+    symbol = "AM";
+  } else if (+hours === 12) symbol = "PM";
+  else {
+    symbol = "PM";
+    hours = (+hours - 12).toString();
+  }
+  return `${hours}:${minutes}:${seconds} ${symbol}`;
+}
