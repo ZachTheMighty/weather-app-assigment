ff7c9011db5b95f99398d659e1444ba2116a8ded
ZachTheMighty <promoted66@gmail.com>
2026-06-26 18:24:15 +0000

    add: days view constructor

---
+++ src/modules/views/days_view.js
@@ -0,0 +1,8 @@
+export default class {
+  constructor() {
+    this.form = document.querySelector("form");
+
+    this.app = document.createElement("days");
+    this.app.classList.add("days");
+  }
+}
