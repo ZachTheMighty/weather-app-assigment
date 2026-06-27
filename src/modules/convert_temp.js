4bf039c4e0c07ca0ba0d25588a510440e325737f
ZachTheMighty <promoted66@gmail.com>
2026-06-27 13:18:34 +0000

    refactor: merge two functions into one convertTemp function

--- src/modules/convert_temp.js
+++ src/modules/convert_temp.js
@@ -1,7 +1,5 @@
-export function convertToFah(temp) {
-  return temp * (9 / 5) + 32;
-}
-
-export function convertToCel(temp) {
-  return (temp - 32) * (5 / 9);
+export default function (temp, unit) {
+  if (unit === "c") return temp * (9 / 5) + 32;
+  else if (unit === "f") return (temp - 32) * (5 / 9);
+  else throw new Error("invalid temp unit, bitch");
 }
