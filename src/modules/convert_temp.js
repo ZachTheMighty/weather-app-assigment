693fc177c7b0064815a46778af4a25a83402a1c0
ZachTheMighty <promoted66@gmail.com>
2026-06-27 13:26:49 +0000

    change: floored returned converted temp

--- src/modules/convert_temp.js
+++ src/modules/convert_temp.js
@@ -1,5 +1,5 @@
 export default function (temp, unit) {
-  if (unit === "c") return temp * (9 / 5) + 32;
-  else if (unit === "f") return (temp - 32) * (5 / 9);
+  if (unit === "c") return Math.floor(temp * (9 / 5) + 32);
+  else if (unit === "f") return Math.floor((temp - 32) * (5 / 9));
   else throw new Error("invalid temp unit, bitch");
 }
