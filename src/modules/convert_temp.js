b3517904757d3b76a9655d9283b3df562ff0d241
ZachTheMighty <promoted66@gmail.com>
2026-06-27 13:43:25 +0000

    fix: converted temps returned were inconsistent
    it had to do with decimals and shit it was confusing but I fixed it but flooring the passed in temp and then rounding the final result

--- src/modules/convert_temp.js
+++ src/modules/convert_temp.js
@@ -1,5 +1,5 @@
 export default function (temp, unit) {
-  if (unit === "c") return Math.floor(Math.floor(temp) * (9 / 5) + 32);
-  else if (unit === "f") return Math.floor((Math.floor(temp) - 32) * (5 / 9));
+  if (unit === "c") return Math.round(Math.floor(temp) * (9 / 5) + 32);
+  else if (unit === "f") return Math.round((Math.floor(temp) - 32) * (5 / 9));
   else throw new Error("invalid temp unit, bitch");
 }
