ebee8df1d100e11342ab4e6b2262d6e8794236c3
ZachTheMighty <promoted66@gmail.com>
2026-06-27 10:37:10 +0000

    add: logic to convert back to 24 hours system to convert time

--- src/modules/convert_to_am_pm.js
+++ src/modules/convert_to_am_pm.js
@@ -1,20 +1,36 @@
 export default function fn(time, noColon) {
   const splitTime = time.split(":");
-  let [hours] = splitTime;
-  let symbol;
 
-  if (+hours > 0 && +hours < 12) symbol = "AM";
-  else if (+hours === 0) {
-    hours = "12";
-    symbol = "AM";
-  } else if (+hours === 12) symbol = "PM";
-  else {
-    symbol = "PM";
-    hours = (+hours - 12).toString();
+  if (!time.includes("AM") && !time.includes("PM")) {
+    let [hours] = splitTime;
+    let symbol;
+
+    if (+hours > 0 && +hours < 12) symbol = "AM";
+    else if (+hours === 0) {
+      hours = "12";
+      symbol = "AM";
+    } else if (+hours === 12) symbol = "PM";
+    else {
+      symbol = "PM";
+      hours = (+hours - 12).toString();
+    }
+
+    if (hours[0] === "0") hours = hours.slice(1);
+
+    if (noColon) return `${hours} ${symbol}`;
+    return `${hours}:00 ${symbol}`;
+  } else {
+    let [hours] = splitTime;
+
+    if (time.includes("AM")) {
+      hours.padStart(2, "0");
+
+      return `${hours}:00:00`;
+    } else {
+      hours = (Number(hours) + 12).toString();
+      hours.padStart(2, "0");
+
+      return `${hours}:00:00`;
+    }
   }
-
-  if (hours[0] === "0") hours = hours.slice(1);
-
-  if (noColon) return `${hours} ${symbol}`;
-  return `${hours}:00 ${symbol}`;
 }
